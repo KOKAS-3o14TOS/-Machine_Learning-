86c944a3d81c47b09f0284032bbdf26cca5dfa61
MR.KOKAS <88835939+KOKAS-3o14TOS@users.noreply.github.com>
2024-09-07 22:15:15 -0600

    Update REGRESION_LINEAL_FRAMEWORK.py

--- REGRESION_LINEAL/REGRESION_LINEAL_FRAMEWORK.py
+++ REGRESION_LINEAL/REGRESION_LINEAL_FRAMEWORK.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 import joblib
-
+import time
 # Cargar datos
 X_train, y_train, X_val, y_val, X_test, y_test = joblib.load('K:\\😈😈😈\\AI\\CIENCIA_DATOS\\PRACTICA\\PROJECT\\DATASET\\DATASET\\RETAIL_SALES\\dataset.pkl')
 
@@ -25,8 +25,10 @@ X_test = scaler.transform(X_test)
 
 # Crear y entrenar el modelo de regresión lineal
 model = LinearRegression()
+start_time = time.time() 
 model.fit(X_train, y_train)
-
+end_time = time.time() 
+training_time = end_time - start_time
 # Hacer predicciones
 y_train_pred = model.predict(X_train)
 y_val_pred = model.predict(X_val)
@@ -41,27 +43,64 @@ val_r2 = r2_score(y_val, y_val_pred)
 test_r2 = r2_score(y_test, y_test_pred)
 
 # Mostrar resultados
+print('MODELO 2')
+print('----MSE----')
 print("Train MSE:", train_mse)
 print("Validation MSE:", val_mse)
 print("Test MSE:", test_mse)
-
+print('----R^2----')
 print("Train R^2:", train_r2)
 print("Validation R^2:", val_r2)
 print("Test R^2:", test_r2)
+print('----Time----')
+print(f"Seg: {training_time:.2f}")
+
+
+plt.tight_layout()
+metrics = ['MSE (Train)', 'MSE (Validation)', 'MSE (Test)']
+values = [train_mse, val_mse, test_mse]
+colors = ['blue', 'green', 'red']
+plt.bar(metrics, values, color=colors)
+plt.axhline(y=train_mse, color='b', linestyle='--')
+plt.axhline(y=val_mse, color='g', linestyle='--')
+plt.axhline(y=test_mse, color='r', linestyle='--')
+plt.ylabel('Valor')
+plt.title('Métricas de MSE')
+plt.show()
+
+plt.tight_layout()
+plt.figure(figsize=(8, 5))
+metrics2 = [ 'R^2 (Train)', 'R^2 (Validation)', 'R^2 (Test)']
+values2 = [ train_r2, val_r2, test_r2]
+colors2 = [ 'blue', 'green', 'red']
+plt.bar(metrics2, values2, color=colors2)
+plt.axhline(y=train_r2, color='b', linestyle='--')
+plt.axhline(y=val_r2, color='g', linestyle='--')
+plt.axhline(y=test_r2, color='r', linestyle='--')
+plt.ylabel('Valor')
+plt.title('Métricas de  R^2')
+plt.show()
+
 
 plt.figure(figsize=(10, 6)) 
 plt.plot(y_val, label='Valores Reales (y_val)', color='blue', linestyle='-', marker='o', markersize=4)  
-plt.plot(y_val_pred, label='Predicciones (y_val_pred)', color='orange', linestyle='--', marker='x', markersize=4) 
-
-plt.title('Comparación de Valores Reales vs Predicciones', fontsize=16, fontweight='bold')
+plt.plot(y_val_pred, label='Predicciones (y_val_pred)', color='green', linestyle='--', marker='x', markersize=4) 
+plt.title('Comparación de Valores Reales vs Predicciones VALIDATION', fontsize=16, fontweight='bold')
 plt.xlabel('Índice de Datos', fontsize=12)
 plt.ylabel('Ventas al por Menor (Retail Sales)', fontsize=12)
-
 plt.legend(loc='best', fontsize=12)  # 'best' ajusta la leyenda en la mejor posición
-
-
 plt.grid(True, linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
 
 
+plt.figure(figsize=(10, 6)) 
+plt.plot(y_test, label='Valores Reales (y_test)', color='blue', linestyle='-', marker='o', markersize=4)  
+plt.plot(y_test_pred, label='Predicciones (y_test_pred)', color='green', linestyle='--', marker='x', markersize=4) 
+plt.title('Comparación de Valores Reales vs Predicciones TEST', fontsize=16, fontweight='bold')
+plt.xlabel('Índice de Datos', fontsize=12)
+plt.ylabel('Ventas al por Menor (Retail Sales)', fontsize=12)
+plt.legend(loc='best', fontsize=12)  # 'best' ajusta la leyenda en la mejor posición
+plt.grid(True, linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
